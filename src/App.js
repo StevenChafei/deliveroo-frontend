88a87f868266de8965134d9111f007f6ab5acf6d
StevenChafei <chafei.s@live.fr>
2023-02-06 17:57:35 +0100

    second save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/img/logo.png";
+import header from "./assets/img/header-image.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} alt="logo de Deliveroo" />
       </header>
+
+      <div className="bandeau">
+        <div className="intro">
+          <h1>Le Pain Quotidien - Montorgueil</h1>
+
+          <p>
+            Profitez de chaque plaisir de la vie quotidienne. Le Pain Quotidien
+            propose des ingrédients simples et sains, du bon pain, des fruits et
+            des légumes frais et de saison issus de l’agriculture biologique.
+          </p>
+
+          <img src={header} alt="photo de l'intro" />
+        </div>
+      </div>
+      <section className="content">
+        <div className="main">
+          <h2>Brunchs</h2>
+          <div className="menu"></div>
+        </div>
+      </section>
     </div>
   );
 }
